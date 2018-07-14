e3db735ac52d4251053566663bb6bc1c04ecc3d6
solomon <shaminsolomonit@gmail.com>
2018-07-14 08:50:34 +0530

    second commit adding messages

--- src/main/java/CircularQueue.java
+++ src/main/java/CircularQueue.java
@@ -1,6 +1,26 @@
 public class CircularQueue {
     private int head = -1;
     private int tail = -1;
-
+    private int[] queue ;
     private static final int CAPACITY = 16;
+
+    public  CircularQueue() {
+        queue = new int[CAPACITY];
+    }
+
+    public void  enqueue() {
+
+    }
+
+    public void dequeue() {
+
+    }
+
+    public boolean isempty() {
+
+    }
+
+    public boolean isfull() {
+
+    }
 }
