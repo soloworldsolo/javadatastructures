65374f6f0d8faae25784e4bdee48ea3fd02cd24b
solomon <shaminsolomonit@gmail.com>
2018-07-15 07:18:03 +0530

    implementing basic queue

--- src/main/java/CircularQueue.java
+++ src/main/java/CircularQueue.java
@@ -8,19 +8,30 @@ public class CircularQueue {
         queue = new int[CAPACITY];
     }
 
-    public void  enqueue() {
+    public void  enqueue(int element) {
+         if(isfull()) throw new ArrayStoreException("queue is full");
+         if(isempty()) head++;
+         queue[++tail] = element;
 
     }
 
     public void dequeue() {
+         if(isempty()) throw new ArrayIndexOutOfBoundsException("no elelments found");
+         queue[head++] = -1;
+
 
     }
 
     public boolean isempty() {
-
+     return (head ==-1 && tail==-1);
     }
 
     public boolean isfull() {
+             return (head+1  == nextpointerisheade());
+    }
 
+    public int nextpointerisheade() {
+        if(++tail == CAPACITY) return  0;
+         return  tail;
     }
 }
